0be5f94e07379234ace79d090db4560c2611e3b5
Zafer Çoban <69654137+zafercobannn@users.noreply.github.com>
2025-08-02 19:51:14 +0300

    Revert to PNG format for photos

--- src/utils/imageUtils.ts
+++ src/utils/imageUtils.ts
@@ -14,7 +14,7 @@ export const getRepresentativeImage = (name: string): string => {
     .replace(/\s+/g, '_')
     .replace(/[^a-z0-9_]/g, '');
   
-  return `${process.env.PUBLIC_URL}/images/representatives/${cleanName}.jpg`;
+  return `${process.env.PUBLIC_URL}/images/representatives/${cleanName}.png`;
 };
 
 /**
@@ -33,5 +33,5 @@ export const checkImageExists = (imageUrl: string): Promise<boolean> => {
  * Varsayılan avatar URL'si
  */
 export const getDefaultAvatar = (): string => {
-  return `${process.env.PUBLIC_URL}/images/representatives/default.jpg`;
+  return `${process.env.PUBLIC_URL}/images/representatives/default.png`;
 }; 
